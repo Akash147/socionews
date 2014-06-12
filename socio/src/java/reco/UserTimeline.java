f05576b2a1d046c2639846dbbb5b891a083fd39d
prabs15 <sanu.frankboy15@gmail.com>
2014-06-12 16:42:03 +0545

    updated userTimelin.java

--- socio/src/java/reco/UserTimeline.java
+++ socio/src/java/reco/UserTimeline.java
@@ -6,7 +6,11 @@
 
 package reco;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -14,6 +18,7 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
 import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 
 /**
  *
@@ -21,8 +26,16 @@ import twitter4j.auth.AccessToken;
  */
 public class UserTimeline {
     Twitter twitter;
-    AccessToken at;
-    User user;
+    private AccessToken at;
+    private long userID;
+    private String token;
+    private String tokenSecret;
+    private User user;
+    
+    List<Status> statuses = null;
+    ArrayList<String> userTweets = new ArrayList<String>();
+    ArrayList<String> hashTags = new ArrayList<String>();
+        
     private UserTimeline(String AccessToken,String access_token_secret) throws TwitterException{
         twitter = new TwitterFactory().getInstance();
         twitter.setOAuthConsumer("vJ3dChUEdLKrK7ciMdtbcXPjz", "XnbW32jqk9jTTY5ZtLHMh0FY9UNC1Wb1ohC0ehIaWUXfpJereQ");
@@ -30,8 +43,75 @@ public class UserTimeline {
         twitter.setOAuthAccessToken(at);
         user = twitter.verifyCredentials();
     }
-    public void getUserTweets()
+    public UserTimeline(Twitter twitter, RequestToken requestToken, String verifier)
     {
-        
+        try {
+            at = twitter.getOAuthAccessToken(requestToken, verifier);
+        } catch (TwitterException ex) {
+            Logger.getLogger(UserTimeline.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public AccessToken returnAccessToken()
+    {
+        return at;
+    }
+    public void setUser(Twitter twitter)
+    {
+        //gives every information about user access through getUserID and accessToken
+        userID = returnAccessToken().getUserId();
+        token = returnAccessToken().getToken();
+        tokenSecret = returnAccessToken().getTokenSecret();
+        try {
+            user = twitter.showUser(this.getUserID());
+        } catch (TwitterException ex) {
+            Logger.getLogger(CallbackServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public long getUserID()
+    {
+        return userID;
+    }
+    public String getToken()
+    {
+        return token;
+    }
+    public String getTokenSecret()
+    {
+        return tokenSecret;
+    }
+    public User getUser()
+    {
+        return user;
+    }
+    
+    public List<Status> getNormalTweets()
+    {
+        // returns only 20 tweets
+        try {
+            statuses = twitter.getUserTimeline();
+        } catch (TwitterException ex) {
+            Logger.getLogger(CallbackServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return statuses;
+    }
+    public List<Status> getPagedTweets(int i)
+    {
+        // returns tweets of i pages
+        Paging page = new Paging(i,200);
+        try {
+            statuses = twitter.getUserTimeline(page);
+        } catch (TwitterException ex) {
+            Logger.getLogger(CallbackServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return statuses;
+    }
+    public List<Status> getTimedTweets(Date date1, Date date2)
+    {
+        try {
+            statuses = twitter.getFavorites();
+        } catch (TwitterException ex) {
+            Logger.getLogger(UserTimeline.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return statuses;
     }
 }
